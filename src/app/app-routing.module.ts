a08ac5df4d73d3116ead6f1e61e52ace43a3824f
alansouza19 <alan.souza-97@hotmail.com>
2023-12-27 09:42:50 -0300

    Criação da tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/pages/home/home.component';
 import { CreateUpdateBooksComponent } from './modules/components/create-update-books/create-update-books.component';
+import { LoginComponent } from './modules/pages/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'books', component: CreateUpdateBooksComponent },
+  { path: 'login', component: LoginComponent },
   
 ];
 
